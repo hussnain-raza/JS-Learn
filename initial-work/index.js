9f44ed1393f838a52b455531d81128b4674c84c4
hussnain56 <hussnain56@yahoo.com>
2017-01-31 02:16:05 +0500

    adding new changes

--- initial-work/index.js
+++ initial-work/index.js
@@ -1,3 +1,27 @@
-function firstFunction(){
-  	document.getElementById("func-1").innerHTML = "Hurraaaah My First Function is working"
+  /* Define function*/
+  function firstFunction(){
+  	document.getElementById("func-1").innerHTML = "<p> 'Hurraaaah My First Function is working' </p>"
   }
+
+  /* Define veriables*/
+  var x, y, z;
+  x9 = 5;
+  y = 6;
+  z = x9 + y;
+
+  function showText(){
+  	document.write(
+  	"we getting sum of veriables x and y with result"
+  	+ " " + z
+  	+ "<br> <br>"
+  	+ "<strong> wao</strong> "
+  	+ "<br> <br>"
+  	);
+  }
+
+  var x, y, z;
+
+  x = 100;
+  y = 80;
+  z =x % y; 
+  console.log(z)
