982885da34d67fbb2124a79897b0e1b87881acc3
hussnain raza <hussnain.raza@arbisoft.com>
2017-06-01 22:32:21 +0500

    make files directory

---
+++ initial-work/JS/index.js
@@ -0,0 +1,37 @@
+  /* Define function*/
+<!doctype html>
+
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="style.css?v=1.0">
+    <script type="text/javascript" src="index.js"></script>
+
+    <script type="text/javascript">
+        //display methods are the followings
+    </script>
+
+
+    <!--[if lt IE 9]>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+      <![endif]-->
+
+</head>
+
+<body>
+
+
+    <section class="section">
+        <div class="section-wrap">
+            <button></button>
+        </div>
+    </section>
+
+</body>
+
+</html>
